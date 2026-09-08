b5455d5608c59789d7af2c1a41178292056089b9
SaiMokshagna <mokshagnamodala5124@gmail.com>
2026-09-08 23:23:16 +0530

    ATM & Calculator

---
+++ Day-24/Calculator/main.py
@@ -0,0 +1,29 @@
+'''import logic
+
+logic.add(2,5)
+logic.div(8,4)
+logic.sub(43,4)
+logic.mul(89,4)
+logic.exp(2,46)
+logic.rem(2,3)
+
+import logic as lg
+
+lg.add(2,5)
+lg.div(8,4)
+lg.sub(43,4)
+lg.mul(89,4)
+lg.exp(46,2)
+lg.rem(2,3)
+
+from logic import add,sub
+add(5,7)
+sub(9,15)'''
+
+from logic import *
+add(2,5)
+div(8,4)
+sub(43,4)
+mul(89,4)
+exp(46,2)
+rem(2,3)
