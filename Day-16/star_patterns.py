8cba3b13d05bacaa42e897497a4152f38c7a24da
SaiMokshagna <mokshagnamodala5124@gmail.com>
2026-09-08 23:17:42 +0530

    Star patterns

---
+++ Day-16/star_patterns.py
@@ -0,0 +1,136 @@
+'''n=int(input("enter the range: "))
+for i in range(n):
+    for sp in range(n-i-1):
+        print(' ',end=' ')
+    for j in range(i+1):
+        print('*',end=' ')
+    print()
+    
+n=int(input("Enter the range: "))
+for i in range(n):
+    for sp in range(i):
+        print(' ',end=' ')
+    for j in range(n-i):
+        print('*',end=' ')
+    print()
+    
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1 or j==0 or j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or i==n-1  or i==n//2 or j==n//2 or j==0 or j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==j or i+j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+#A   
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n//2 or j==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+  
+#C
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    
+#E
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or i==n//2:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    
+#F
+n=int(input("Enter the range: "))
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n//2:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+#G
+n=int(input("Enter the range: "))
+m=n//2
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 or (j==n-1 and i>=m) or (i==n//2 and j>=m):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+    
+
+n=int(input('Enter the values: '))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==0 or i==n-1 and j<=m or j==m and i>=m or i==m and j>=m or j==n-1 and i>=m:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+
+#j
+n=int(input('Enter the values: '))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if i==0 or j==n//2 or (j<m and i==n-1):
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
+ 
+#K   
+n=int(input('Enter the values: '))
+m = n//2
+for i in range(n):
+    for j in range(n):
+        if j==0 or (i==m and j<=m) or i+j==n-1 and i<=m or i==j and i>m:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()'''
+    
+#L
+n=int(input('Enter the values: '))
+for i in range(n):
+    for j in range(n):
+        if j==0 or i==n-1 and j!=n-1:
+            print('*',end=' ')
+        else:
+            print(' ',end=' ')
+    print()
